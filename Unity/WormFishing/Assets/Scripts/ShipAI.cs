c0a791ac65524d03118282b6d0c0f2aa8ebc1cb3
kishoreven1729 <kompeshan1729@gmail.com>
2014-05-10 06:56:48 -0700

    Starting on AI

---
+++ Unity/WormFishing/Assets/Scripts/ShipAI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipAI : MonoBehaviour
+{
+    #region Private Variables
+    private Vector3         _target;
+
+    private float           _movementSpeed;
+    private float           _switchTargetTimer;
+    #endregion
+
+    #region Public Variables
+    public float            switchTargetInterval;
+
+    private Vector3 _leftBoundary;
+    private Vector3 _rightBoundary;
+    #endregion
+
+    #region Constructor
+    void Start () 
+    {
+	    
+	}
+    #endregion
+
+    #region Loop
+    void Update () 
+    {
+
+    }
+    #endregion
+
+    #region Methods
+    #endregion
+}
