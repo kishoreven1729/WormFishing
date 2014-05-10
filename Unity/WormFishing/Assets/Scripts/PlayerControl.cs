46b4c692640c5ca141d0cfa4cce6d7a468d7e0f4
kishoreven1729 <kompeshan1729@gmail.com>
2014-05-09 17:14:39 -0700

    Added the rope mechanics

---
+++ Unity/WormFishing/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿#region References
+using UnityEngine;
+using System.Collections;
+#endregion
+
+public class PlayerControl : MonoBehaviour 
+{
+	#region Private Variables
+	#endregion
+
+	#region Public Variables
+	#endregion
+
+	#region Constructor
+	void Start() 
+	{
+		rigidbody.AddForce (new Vector3 (2000.0f, 0.0f, 0.0f));
+	}
+	#endregion
+	
+	#region Loop
+	void Update() 
+	{
+	
+	}
+	#endregion
+}
