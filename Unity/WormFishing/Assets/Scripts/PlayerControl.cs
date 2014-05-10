8659ad0315581aa4250593cd8c85566e157a37fe
kishoreven1729 <kompeshan1729@gmail.com>
2014-05-09 21:28:03 -0700

    Rope basics and character hanging done

--- Unity/WormFishing/Assets/Scripts/PlayerControl.cs
+++ Unity/WormFishing/Assets/Scripts/PlayerControl.cs
@@ -6,22 +6,36 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour 
 {
 	#region Private Variables
+	private ConstantForce		_constantForce;
 	#endregion
 
 	#region Public Variables
+	public float 				magnitude;
 	#endregion
 
 	#region Constructor
 	void Start() 
 	{
-		rigidbody.AddForce (new Vector3 (2000.0f, 0.0f, 0.0f));
+		//rigidbody.AddForce (new Vector3 (1000.0f, 0.0f, 0.0f));
+		_constantForce = GetComponent<ConstantForce> ();
 	}
 	#endregion
 	
 	#region Loop
 	void Update() 
 	{
-	
+		Vector3 direction = Vector3.zero;
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			direction.x = 1.0f;
+		}
+		else if(Input.GetKey(KeyCode.D))
+		{
+			direction.x = -1.0f;
+		}
+
+		_constantForce.force = direction * magnitude;
 	}
 	#endregion
 }
