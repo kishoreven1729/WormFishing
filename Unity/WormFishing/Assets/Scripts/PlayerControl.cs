e9189db94abf97f6ad3d647a135d8ef812426153
kishoreven1729 <kompeshan1729@gmail.com>
2014-05-10 15:41:51 -0700

    Player control is now functional!

--- Unity/WormFishing/Assets/Scripts/PlayerControl.cs
+++ Unity/WormFishing/Assets/Scripts/PlayerControl.cs
@@ -6,18 +6,28 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour 
 {
 	#region Private Variables
-	private ConstantForce		_constantForce;
+    private float               _gestureFactor;
+
+    private Vector2             _startPos;
+    private Vector2             _endPos;
 	#endregion
 
 	#region Public Variables
-	public float 				magnitude;
+	public float 				forceMagnitude;
+    public float                forceFluctuation;
+    public float                minGestureDistance;
+    public float                maxGestureDistance;
 	#endregion
 
 	#region Constructor
 	void Start() 
 	{
-		//rigidbody.AddForce (new Vector3 (1000.0f, 0.0f, 0.0f));
-		_constantForce = GetComponent<ConstantForce> ();
+		//rigidbody.AddForce (new Vector3 (1000.0f, 0.0f, 0.0f));		
+
+        _gestureFactor = forceFluctuation / (maxGestureDistance - minGestureDistance) ;
+
+        _startPos = Vector2.zero;
+        _endPos = Vector2.zero;
 	}
 	#endregion
 	
@@ -26,14 +36,64 @@ public class PlayerControl : MonoBehaviour
 	{
 		Vector3 direction = Vector3.zero;
 
-		if(Input.GetKey(KeyCode.A))
+        #region Keyboard
+        if (Input.GetKeyDown(KeyCode.A))
+		{
+			direction.x = -1.0f;            
+		}
+		if(Input.GetKeyDown(KeyCode.D))
 		{
 			direction.x = 1.0f;
 		}
-		else if(Input.GetKey(KeyCode.D))
-		{
-			direction.x = -1.0f;
-		}		
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            direction.y = 1.0f;
+        }
+
+        rigidbody.AddForce(direction * forceMagnitude);
+        #endregion
+
+        #region Touch
+        if (Input.touchCount > 0)
+        {
+            Touch currentTouch = Input.GetTouch(0);            
+
+            switch (currentTouch.phase)
+            {
+                case TouchPhase.Began:
+                    {
+                        _startPos = currentTouch.position;                       
+
+                        break;
+                    }
+                case TouchPhase.Ended:
+                    {
+                        _endPos = currentTouch.position;
+
+                        float moveDistance = Vector2.Distance(_startPos, _endPos);
+
+                        if (moveDistance > minGestureDistance)
+                        {
+                            if (moveDistance > maxGestureDistance)
+                            {
+                                moveDistance = maxGestureDistance;
+                            }
+
+                            float magnitudeModifier = (moveDistance - minGestureDistance) * _gestureFactor;
+
+                            direction.x = _endPos.x - _startPos.x;
+                            direction.y = _endPos.y - _startPos.y;
+
+                            float newMagnitude = forceMagnitude + magnitudeModifier;
+
+                            rigidbody.AddForce(direction * newMagnitude);
+                        }
+
+                        break;
+                    }
+            }            
+        }
+        #endregion        
 	}
 	#endregion
 
