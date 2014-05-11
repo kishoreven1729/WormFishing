0aaa9a6ee10b3b1cdd7e3198f6420cba5a6136b1
kishoreven1729 <kompeshan1729@gmail.com>
2014-05-10 21:19:03 -0700

    WormAI almost done!

---
+++ Unity/WormFishing/Assets/Scripts/GameDirector.cs
@@ -0,0 +1,43 @@
+﻿#region References
+using UnityEngine;
+using System.Collections;
+#endregion
+
+public class GameDirector : MonoBehaviour 
+{
+    #region Private Variables
+    #endregion
+
+    #region Public Variables
+    public Transform            character;
+    public static GameDirector  instance;
+    #endregion
+
+    #region Constructor
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+    #endregion
+
+    #region Loop
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region Private Methods
+    #endregion
+
+    #region Public Methods
+    #endregion
+}
--- Unity/WormFishing/Assets/Scripts/WormAI.cs
+++ Unity/WormFishing/Assets/Scripts/WormAI.cs
@@ -1,15 +1,68 @@
-﻿using UnityEngine;
+﻿#region References
+using UnityEngine;
 using System.Collections;
+#endregion
 
-public class WormAI : MonoBehaviour {
+public class WormAI : MonoBehaviour
+{
+    #region Private Variables
+    private float           _fireTimer;
+    private bool            _canFire;
+    private WormControl     _wormControl;
+    #endregion
 
-	// Use this for initialization
-	void Start () {
-	
+    #region Public Variables
+    public float       fireInterval;    
+    #endregion
+
+    #region Constructor
+    void Start () 
+    {
+        _fireTimer = Time.time + fireInterval;
+
+        _canFire = true;
+
+        _wormControl = transform.GetChild(0).GetComponent<WormControl>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    #endregion
+
+    #region Loop
+    void Update () 
+    {
+        if(Time.time > _fireTimer)
+        {
+            if(_canFire == true)
+            {
+                AdjustOrientation();
+
+                _wormControl.PlayAnimation("Pullup");
+
+                _canFire = false;
+            }
+        }
+    }
+    #endregion
+
+    #region Private Methods
+    private void AdjustOrientation()
+    {
+        Vector3 characterPosition = GameDirector.instance.character.position;
+        characterPosition.z = transform.position.z;
+
+        Vector3 targetVector =  characterPosition - transform.position;
+
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, targetVector);
+    }
+    #endregion
+
+    #region Public Methods
+    public void AnimationEnded()
+    {
+        _canFire = true;
+
+        transform.rotation = Quaternion.identity;
+
+        _fireTimer = Time.time + fireInterval;
+    }
+    #endregion
 }
